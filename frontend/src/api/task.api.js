31810d01b9f139a24d05583bbeb73d7e1d6f0bbf
normanb <bautistanorman2005@gmail.com>
2026-01-12 20:44:04 +0800

    WIP: Task Modal and Task Creation

--- frontend/src/api/task.api.js
+++ frontend/src/api/task.api.js
@@ -1,8 +1,7 @@
-import api from axios_Instance.js
+import api from 'axios_Instance.js'
 
 export const get_All_Tasks = async () => {
     try { 
-        setIsLoading(true);
         const response = await api.get("api/v1/task/tasks");
         return response.data;
     } catch (error) {
--- frontend/src/shared/components/TodoModal.jsx
+++ frontend/src/shared/components/TodoModal.jsx
@@ -4,14 +4,51 @@ import { create_Task, update_Task, delete_Task } from '../../api/task.api'
 
 
 const TodoModal = ({onClose,disptach}) => {
-  return (
-
-    const [timer, setTimer] = useState({
+  
+  const [timer, setTimer] = useState({
       
-    })
+    });
 
-    <div>
-    
+
+  return (
+    <div className="container fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+      <div className='bg-secondary rounded-2xl shadow-lg p-8 w-full max-w-md'>
+        <h2 className='text-md font-bold text-primary '>
+          Add a Task 
+        </h2>
+        <div className='space-y-4'>
+          <div className="container">
+            <form action="">
+              <label>
+              Title:
+            </label>
+            <input
+              type="text"
+              name="Task Title"
+              className='w-full border rounded px-3 py-2'
+            />
+            <label>
+              Description:
+            </label>
+            <input
+              type="text"
+              name="Task Title"
+              className='w-full border rounded px-3 py-2'
+            />
+            <label>
+              Priority:
+            </label>
+            <select name="status" id="">
+              <option value="High">High Priority/Urgent</option>
+              <option value="Moderate">Medium Prioriry/Moderate</option>
+              <option value="Low">Low Priority/Minor</option>
+            </select>
+            
+            </form>
+            
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
